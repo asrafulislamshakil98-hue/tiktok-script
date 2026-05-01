db73d842053c77f1bb38d83b74f54835ad586d5a
asrafulislamshakil98-hue <asrafulislamshakil98@gmail.com>
2026-05-01 12:26:23 -0700

    Add files via upload

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
+const path = require('path'); 
 require('dotenv').config();
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+app.use(express.static(path.join(__dirname, 'frontend'))); 
 
 mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB Connected..."))
